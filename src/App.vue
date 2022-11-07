a2b78a80872661ba3b0fc910b9e13d6cbc11949d
Bence Hornyák <sir.bence15@gmail.com>
2022-11-07 16:30:17 +0100

    refactor: 💡 options example
    BREAKING CHANGE: 🧨 Trigger new deploy

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import useGeolocation from "./hooks/vx-geolocation";
 
-const { position, error } = useGeolocation();
+const { position, error } = useGeolocation({ enableHighAccuracy: true });
 </script>
 
 <template>
